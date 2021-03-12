9f7ae090088f1f60b0239983e93c652825030f9b
Christopher Burch <christopher.burch2@gmail.com>
2021-03-11 19:32:29 -0600

    Add project files.

---
+++ MlbTheShow20 Stat Console App/Pitch.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mlb20TheShow_Stat_Randomizer
+{
+    public enum PitchType
+    {
+      None,
+      FourSeam,
+      TwoSeam,
+      Slider,
+      Curveball,
+      Changeup,
+      Cutter,
+      CircleChange,
+      Palmball,
+      Forkball,
+      Knucklecurve,
+      Screwball,
+      TwelveSixCurve,
+      SweepingCurve,
+      RunningFB,
+      VulcanChange,
+      Sinker,
+      Slurve,
+      Splitter,
+      Knunckleball,
+    }
+
+    public class Pitch
+    {
+        public PitchType PitchType { get; set; }
+        public int Velocity { get; set; }
+        public int Control { get; set; }
+        public int Break { get; set; }
+
+        public Pitch(Random random)
+        {
+            Velocity = random.Next(0, 101);
+            Control = random.Next(0, 101);
+            Break = random.Next(0, 101);
+        }
+    }
+}
